d7ab8741904a6146b6acd9efef819ca51bcc399e
RendyAFS <99011571+RendyAFS@users.noreply.github.com>
2023-06-23 14:35:01 +0700

    Add files via upload

---
+++ storage/framework/views/59f406323e6635fb2b7aaea32d544ba0.php
@@ -0,0 +1,25 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo e(__('You are logged in!')); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\----- 1. Kuliah\2. Bahan - bahan Kuliah\✨ SEMESTER 4\PEMROGRAMAN FRAMEWORK - IS-04-04\Pertemuan 10\modul-8\resources\views/home.blade.php ENDPATH**/ ?>
